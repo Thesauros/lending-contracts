f782d87fbaef4d98aafebaf6d8a3097fafd14fd0
Levon Avetisyan <avelevon23@gmail.com>
2024-06-25 18:23:17 +0400

    audit: use Ownable2Step instead of Ownable in InterestLocker

--- contracts/InterestLocker.sol
+++ contracts/InterestLocker.sol
@@ -10,9 +10,9 @@ pragma solidity 0.8.23;
 
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
 
-contract InterestLocker is Ownable {
+contract InterestLocker is Ownable2Step {
     using SafeERC20 for IERC20;
 
     /**
