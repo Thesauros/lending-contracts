0536ce1691156a4af89b9e47f01085ebc75c7314
avelevon <avelevon23@gmail.com>
2025-02-26 20:25:19 +0400

    update aaveV3 interfaces

--- contracts/interfaces/aaveV3/IPool.sol
+++ contracts/interfaces/aaveV3/IPool.sol
@@ -1,11 +1,11 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.23;
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.0;
 
 /**
  * @title IPool
  * @author Aave
  * @notice Defines the basic interface for an Aave Pool.
- * @dev Reduced version of the original Aave interface.
+ * @dev Modified version of the original Aave interface.
  */
 interface IPool {
     struct ReserveConfigurationMap {
